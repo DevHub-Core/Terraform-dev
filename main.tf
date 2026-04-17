20d29b75f8e83106f7ee69e0b3ce698f53997bcb
ByFlowt <tobpro295@gmail.com>
2026-04-18 00:03:41 +0200

    add dependabot

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "github" {
   token = var.github_token
-  owner = var.github_org   # your GitHub username for personal testing
+  owner = var.github_org
 }
